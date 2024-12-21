cb3a7ef492ea3886c1efb0976284671c1b0afe68
fazza-tech <ebranfazza@gmail.com>
2024-12-21 06:57:28 +0530

    updated Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
 import React from 'react';
 
-
-
 const App = () => (
-  <MainSection/>
+  <MainSection />
 
 );
 
-function MainSection(){
-  return(
-  <div>
-    <header>
-       <img src="src/assets/react.svg" width="100px" alt="react logo" />
-    </header>
-   <main>
-     <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was fiest released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 200k stars on github</li>
-      <li>Is maintained by Meta</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-    <h2>Why i'm learning react in 2024?</h2>
-    <ol>
-      <li>React is a popular library, so i can new cool dev's out there😁</li>
-      <li>I'm more likely to get a job as a front-end developer if i know react</li>
-    </ol>
-   </main>
-   <footer>
-    <small>© 2024 Karbaala development. All rights reserved</small>
-   </footer>
-   
-  </div>
-    
+function MainSection() {
+  return (
+    <>
+      <header>
+        <img src="src/assets/react.svg" width="100px" alt="react logo" />
+      </header>
+      <main>
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Was fiest released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 200k stars on github</li>
+          <li>Is maintained by Meta</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+        <h2>Why i'm learning react in 2024?</h2>
+        <ol>
+          <li>React is a popular library, so i can new cool dev's out there😁</li>
+          <li>I'm more likely to get a job as a front-end developer if i know react</li>
+        </ol>
+      </main>
+      <footer>
+        <small>© 2024 Karbaala development. All rights reserved</small>
+      </footer>
+
+    </>
+
   )
 }
 
