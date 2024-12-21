791c232722a1d08559d10cbc77aa804667520a8e
fazza-tech <ebranfazza@gmail.com>
2024-12-21 17:28:03 +0530

    Updated all

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+
+export default function Footer() {
+    return (
+      <footer className='footer'>
+        <small>© 2024 Karbaala development. All rights reserved</small>
+      </footer>
+    )
+  }
+  
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-
 import { createRoot } from 'react-dom/client'
 
-import Page from './App.jsx'
-
-
+import Header  from './Header.jsx'
+import MainContent from './MainCOntent.jsx'
+import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
-  
-    <Page /> 
-  
+    <>
+    <Header/>
+    <MainContent/>
+    <Footer/>
+    </>
 )
